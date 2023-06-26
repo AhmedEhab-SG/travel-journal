b4907d29eb11d98209e49dfdc2cabb713e0baa83
Ahmed Ehab <ahmed.ehab632@gmail.com>
2023-06-27 01:51:41 +0300

    update

--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,19 @@
-import "./style.css"
+import "./style.css";
 import Header from "./header";
 import Post from "./post";
-import Data from "./data"
+import Data from "./data";
 
+export default function App() {
+  const visitLocation = Data.map((post) => (
+    <Post key={post.title} post={post} />
+  ));
 
-export default function App(){
-
-    const visitLocation = Data.map(post => <Post
-        key = {post.title}
-        post = {post}
-    />)
-
-    return(
-        <div className="container">
-            <div className="journal-container">
-                <Header/> 
-                {visitLocation}
-            </div>
-        </div>
-    )
+  return (
+    <div className="container">
+      <div className="journal-container">
+        <Header />
+        {visitLocation}
+      </div>
+    </div>
+  );
 }
--- src/data.js
+++ src/data.js
@@ -1,32 +1,37 @@
 const data = [
-    {
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2022",
-        endDate: "24 Jan, 2022",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
-    },
-    {   
-        title: "Sydney Opera House",
-        location: "Australia",
-        googleMapsUrl: "https://goo.gl/maps/WJh9isym2GVYNKvJ6",
-        startDate: "27 May, 2022",
-        endDate: "27 hun, 2022",
-        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney Locacted on the banks of the Sydney Harbour, its is often regarded as one of the 20th century's most famous and distinctive buldings",
-        imageUrl: "https://lh5.googleusercontent.com/p/AF1QipMlSLQH7joE8WSEAb-uEXp5gfIijNOa_AsNwBNe=w408-h255-k-no"
-    },
-    {
-        id: 1,
-        title: "Geirangerfjord",
-        location: "Norway",
-        googleMapsUrl: "https://goo.gl/maps/MjBB8DQhCf4J2gH88",
-        startDate: "1 Oct, 2022",
-        endDate: "18 nov, 2022",
-        description: "The Geiranger Fjord in the Summers region of Mare Og Romsdal country, Norway. It is located entirely in the Stranda Muncipality",
-        imageUrl: "https://lh5.googleusercontent.com/p/AF1QipO8DL_SU_umnbwBCKXiPoiTmulegaemweb827G8=w426-h240-k-no"
-    }
-]
+  {
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2022",
+    endDate: "24 Jan, 2022",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+  },
+  {
+    title: "Sydney Opera House",
+    location: "Australia",
+    googleMapsUrl: "https://goo.gl/maps/WJh9isym2GVYNKvJ6",
+    startDate: "27 May, 2022",
+    endDate: "27 hun, 2022",
+    description:
+      "The Sydney Opera House is a multi-venue performing arts centre in Sydney Locacted on the banks of the Sydney Harbour, its is often regarded as one of the 20th century's most famous and distinctive buldings",
+    imageUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipMlSLQH7joE8WSEAb-uEXp5gfIijNOa_AsNwBNe=w408-h255-k-no",
+  },
+  {
+    id: 1,
+    title: "Geirangerfjord",
+    location: "Norway",
+    googleMapsUrl: "https://goo.gl/maps/MjBB8DQhCf4J2gH88",
+    startDate: "1 Oct, 2022",
+    endDate: "18 nov, 2022",
+    description:
+      "The Geiranger Fjord in the Summers region of Mare Og Romsdal country, Norway. It is located entirely in the Stranda Muncipality",
+    imageUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipO8DL_SU_umnbwBCKXiPoiTmulegaemweb827G8=w426-h240-k-no",
+  },
+];
 
-export default data
+export default data;
